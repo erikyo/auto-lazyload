a431add58137d119e1e52774e25a13018df2d40e
Erik Golinelli <erik@codekraft.it>
2024-04-14 12:21:27 +0200

    types for the window glob object

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,25 @@ import {LazyloadOptions} from './types'
 export const fakeSrc = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
 
 /**
+ * The default options for the lazy loading function.
+ */
+const defaults = {
+    loading: 'lazy-loading',
+    failed: 'lazy-failed',
+    on: 'lazy',
+    loaded: 'lazy-loaded',
+    attribute: 'lazy',
+    nativeSupport: false,
+    observer: {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 0
+    }
+}
+
+/**
  * If needed, you can set the lazyloadOptions in the window object before the document is loaded to override the default options
  */
-export const options = 'lazyloadOptions' in window
-    ? window?.lazyloadOptions as LazyloadOptions
-    : {loading: 'lazy-loading', failed: 'lazy-failed', on: 'lazy', loaded: 'lazy-loaded', attribute: 'lazy', nativeSupport: false}
+export const options = 'autolazy' in window
+    ? {...defaults, ...window?.autolazy}
+    : defaults
